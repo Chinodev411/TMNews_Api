de69b4518647f3dbb7304d96ba58c4209d09fea9
Chinodev411 <Chinodev@outlook.com>
2021-08-06 03:03:55 -0400

    Comment model

--- techmount/models.py
+++ techmount/models.py
@@ -7,8 +7,18 @@ class News(models.Model):
     description = models.CharField(max_length=200)
     url = models.CharField(max_length=100)
     source = models.CharField(max_length=100)
-    published = models.CharField(max_length=100)
-    owner = models.ForeignKey('users.User', related_name='techmount', on_delete=models.CASCADE)
+    published = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('users.User', related_name='news', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.author
+
+class Comment(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    news = models.ForeignKey( News, on_delete=models.CASCADE, related_name='comments')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('users.User', related_name='comments', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
