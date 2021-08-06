f8bdf8f04f45f62741fac52319b7a099d65431ed
Chinodev411 <Chinodev@outlook.com>
2021-08-06 10:32:44 -0400

    views.py

--- techmount/views.py
+++ techmount/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .models import News, Comment
+from .serializers import NewsSerializer, CommentSerializer
+from rest_framework import permissions
+from techmount.permissions import IsOwnerOrReadOnly
+
+
+class NewsList(generics.ListCreateAPIView):
+    queryset = News.object.all()
+    serializer_class = NewsSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class NewsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+
