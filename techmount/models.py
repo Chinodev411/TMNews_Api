986fa9bb37b6e64629dc5613c888c2a9f8b064b5
Chinodev411 <Chinodev@outlook.com>
2021-08-06 02:47:07 -0400

    Model News

--- techmount/models.py
+++ techmount/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    url = models.CharField(max_length=100)
+    source = models.CharField(max_length=100)
+    published = models.CharField(max_length=100)
+    owner = models.ForeignKey('users.User', related_name='techmount', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
